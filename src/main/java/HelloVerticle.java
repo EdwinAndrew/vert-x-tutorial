7469d2851e62243b07bb06b3fea6020902fa3757
Edwin Andrew <edwinandrew96@gmail.com>
2020-05-12 13:22:38 -0400

    Update to JUnit 5

--- src/main/java/HelloVerticle.java
+++ src/main/java/HelloVerticle.java
@@ -27,7 +27,6 @@ public class HelloVerticle extends AbstractVerticle {
         router.get("/r2/:word")
                 .handler(this::handleR2);
     }
-
     public static int add(int num1, int num2){
         return num1+num2;
     }
